f41a9c70f9b41c72618e12b8b26d94f871ec12b5
i-havr <i.havrylov@gmail.com>
2024-09-04 12:57:05 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  build: { outDir: "./docs" },
+  base: "./",
+});
